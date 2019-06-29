3041b2d033dbd958298746a7dc9638f4a9c080b7
unknown <arinzeokorji@yahoo.com>
2019-06-29 13:24:06 +0100

    module in progress

---
+++ buildScripts/startMesage.js
@@ -0,0 +1,3 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Dev mode starting')); // eslint-disable-line no-console
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import numerals from 'numeral';
+
+const courseVal = numerals(1000);
+
+console.log(`i have ${courseVal} fro a course`); //eslint-disable-line no-console
